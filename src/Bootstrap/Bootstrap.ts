3e95cb90869d331596e8625011665381416e7337
João Lenon <lenonSec7@gmail.com>
2023-01-29 22:11:40 -0300

    Initial commit

---
+++ src/Bootstrap/Bootstrap.ts
@@ -0,0 +1,17 @@
+/**
+ * @athenna/template
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export class Bootstrap {
+  /**
+   * Executes the application.
+   */
+  static main(...args: string[]): string {
+    return `Bootstrap: ${args.join(' ')}`
+  }
+}
